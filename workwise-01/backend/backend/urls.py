342deae442e3058c3b51bf3a1dd4d719069cd123
aviat-at <akashtharindu7788@gmail.com>
2024-10-11 01:56:30 -0400

    new_backend_app

---
+++ workwise-01/backend/backend/urls.py
@@ -0,0 +1,10 @@
+# backend/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('accounts.urls')),  # Example inclusion of another app's URLs
+]
