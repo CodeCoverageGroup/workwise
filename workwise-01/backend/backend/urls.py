c94c7b5a41f8ef9d1e93dd92a3ae6dc1917dd7a9
glee6973 <glee6973@conestogac.on.ca>
2024-10-27 19:47:33 -0400

    clean: remove some legacy folder and files.

--- workwise-01/backend/backend/urls.py
+++ workwise-01/backend/backend/urls.py
@@ -2,10 +2,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to the homepage!")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),  # Example inclusion of another app's URLs
     path('api/', include('departments.urls')),
+    path('', home_view),
 ]
