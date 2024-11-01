0dbbe20fcf2442d4eb4d9e7c71f59814c192c753
glee6973 <glee6973@conestogac.on.ca>
2024-10-31 22:49:23 -0400

    test: add test for jobs

--- workwise-01/backend/jobs/urls.py
+++ workwise-01/backend/jobs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import JobListView, JobDetailView
 
 urlpatterns = [
-    path('', JobListView.as_view(), name='job_list'),
-    path('<int:id>/', JobDetailView.as_view(), name='job_detail'),
+    path('jobs/', JobListView.as_view(), name='job_list'),
+    path('jobs/<int:id>/', JobDetailView.as_view(), name='job_detail'),
 ]
